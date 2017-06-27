08b55827bb5497f5f94caf5b76dcd42f45eea765
JAKEFELDMAN <jacob.h.feldman@gmail.com>
2017-06-26 18:10:43 -0600

    Template Engine working on user page

---
+++ assets/scripts/user.js
@@ -0,0 +1,90 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+  const API_URL = `http://localhost:3000/api/v1/users/`
+  const hrefLocation = window.location.href;
+  const parsedQueryString = parseQueryString(hrefLocation);
+
+  function parseQueryString(queryString) {
+      queryString = queryString.split('=')
+      return queryString[1];
+  }
+
+  createRequestEndpoint(parsedQueryString);
+
+  function createRequestEndpoint(id) {
+    const ACCOUNT_URL = API_URL + `${id}`;
+    const PROFILE_URL = API_URL + `${id}/profile`;
+    createUserRequest(ACCOUNT_URL);
+    createProfileRequest(PROFILE_URL);
+  }
+
+  function createProfileRequest(PROFILE_URL) {
+    const profileRequest = new Request(PROFILE_URL, {
+      method: "get",
+      mode: 'cors'
+    })
+    getProfileInformation(profileRequest)
+  }
+
+  function getProfileInformation(request) {
+    fetch(request)
+      .then(parseJSON)
+      .then(returnProfileResponse)
+      .catch(throwError)
+  }
+
+  function createUserRequest(ACCOUNT_URL) {
+    const userRequest = new Request(ACCOUNT_URL, {
+      method: "get",
+      mode: 'cors'
+    })
+    getUserInformation(userRequest)
+  }
+
+  function getUserInformation(request) {
+    fetch(request)
+      .then(parseJSON)
+      .then(returnPropertyResponse)
+      .catch(throwError)
+  }
+
+  function parseJSON (response) {
+    return response.json();
+  }
+
+  function throwError() {
+    return new Error("Error")
+  }
+
+  function returnProfileResponse(response) {
+    const profile = response;
+    showUserProfile(profile)
+  }
+
+  function returnPropertyResponse(response) {
+    const property = response;
+    showUserProperty(property)
+  }
+
+  function showUserProfile(profile) {
+    const source = document.querySelector('#user-profile-template').innerHTML;
+    const template = Handlebars.compile(source);
+    const html = template(profile);
+    const getUser = document.querySelector('.profile');
+    const userDiv = document.createElement('div');
+    userDiv.innerHTML = html;
+    getUser.appendChild(userDiv);
+  }
+
+  function showUserProperty(property) {
+    const source = document.querySelector('#user-property-template').innerHTML;
+    const template = Handlebars.compile(source);
+    console.log(property);
+    const html = template({property});
+    console.log(html);
+    const getProperty = document.querySelector('.property');
+    const propertyDiv = document.createElement('div');
+    propertyDiv.innerHTML = html;
+    getProperty.appendChild(propertyDiv);
+  }
+});
