69d1cd879d4cbc61c29ad37d9eed9f15928b880a
JAKEFELDMAN <jacob.h.feldman@gmail.com>
2017-06-28 17:18:37 -0600

    Added Dev Env

--- assets/scripts/property.js
+++ assets/scripts/property.js
@@ -1,5 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  const PROPERTY_ENDPOINT = `https://rocky-shelf-87257.herokuapp.com/api/v1/property/`
+
+  let BASE_URL = '';
+  function getBaseURL() {
+    if (window.location.hostname == "localhost") {
+      BASE_URL = `http://localhost:3000`;
+    } else {
+      BASE_URL = `https://rocky-shelf-87257.herokuapp.com`
+    }
+  }
+
+  getBaseURL();
+
+  const PROPERTY_ENDPOINT = BASE_URL + `/api/v1/property/`
   const hrefLocation = window.location.href;
   const parsedQueryString = parseQueryString(hrefLocation);
   createPropertyEndpoint(parsedQueryString);
